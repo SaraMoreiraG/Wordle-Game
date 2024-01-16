5d45c7ff36a88d29d8245f78bd4efa2868148582
SaraMoreiraG <sara_moreira@hotmail.es>
2024-01-16 17:27:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Keyboard from "./components/keyboard.js";
 
 function App() {
+  const [word, setWord] = useState("");
+  const [currentWord, setCurrentWord] = useState("");
+  const [userWords, setUserWords] = useState([]);
+  const [colorMarkings, setColorMarkings] = useState([]);
+  const [instructions, setInstructions] = useState("Guess the hidden word");
+
+  useEffect(() => {
+    const fetchWord = async () => {
+      try {
+        const response = await fetch(
+          "https://random-word-api.herokuapp.com/word?length=5"
+        );
+        const data = await response.json();
+        setWord(data[0]);
+      } catch (error) {
+        console.error("Error fetching word:", error);
+      }
+    };
+
+    fetchWord();
+  }, []);
+
+  const evaluateSituation = () => {
+    if (userWords.length === 4) {
+      setInstructions(
+        `You loose. The hidden word was: ${word.toUpperCase()}. Try again`
+      );
+      setCurrentWord("");
+      setUserWords([]);
+      setColorMarkings([]);
+    } else {
+      evaluateWord();
+    }
+  };
+  const evaluateWord = () => {
+    if (currentWord.length === 5) {
+      const markings = currentWord.split("").map((letter, index) => {
+        const lowercaseLetter = letter.toLowerCase();
+        if (word[index] === lowercaseLetter) {
+          return { letter, color: "green" }; // Correct letter in the correct spot
+        } else if (word.includes(lowercaseLetter)) {
+          return { letter, color: "yellow" }; // Correct letter in the wrong spot
+        } else {
+          return { letter, color: "red" }; // Incorrect letter
+        }
+      });
+
+      setUserWords((prevUserWords) => [...prevUserWords, currentWord]);
+      setColorMarkings((prevColorMarkings) => [...prevColorMarkings, markings]);
+      setCurrentWord("");
+      setInstructions("Guess the hidden word");
+    } else {
+      setInstructions("Too short");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-view">
+        <div className="row justify-content-center mx-0 pt-5">
+          <div className="light-box solution-panel col-4">
+            <div>
+              {colorMarkings.map((markings, index) => (
+                <div className="letter-box" key={index}>
+                  {markings.map((value, index) => (
+                    <div className={`letter ${value.color}`} key={index}>
+                      <div className="key-content">{value.letter}</div>
+                    </div>
+                  ))}
+                </div>
+              ))}
+              <div className="letter-box">
+                {currentWord.split("").map((letter, index) => (
+                  <div className="letter blue" key={index}>
+                    <div className="key-content">{letter}</div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <p>{instructions}</p>
+          </div>
+        </div>
+        <div className="row justify-content-center m-0">
+          <div className="col-6">
+            <Keyboard
+              currentWord={currentWord}
+              setCurrentWord={setCurrentWord}
+              evaluateSituation={evaluateSituation}
+              setInstructions={setInstructions}
+            />
+          </div>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/keyboard.js
@@ -0,0 +1,133 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
+
+function Keyboard({
+  currentWord,
+  setCurrentWord,
+  evaluateSituation,
+  setInstructions,
+}) {
+  const maxLettersPerWord = 5;
+
+  const handleClick = (letter) => {
+    if (currentWord.length < maxLettersPerWord) {
+      setCurrentWord(
+        (prevCurrentWord) => prevCurrentWord + letter.toUpperCase()
+      );
+    } else {
+      setInstructions(<FontAwesomeIcon icon={faPaperPlane} />);
+    }
+  };
+
+  const handleEnter = () => {
+    evaluateSituation();
+  };
+
+  const handleDelete = () => {
+    if (currentWord.length > 0) {
+      setCurrentWord((prevCurrentWord) => prevCurrentWord.slice(0, -1));
+    }
+  };
+
+  return (
+    <div className="light-box keyboard">
+      <div className="box-inner">
+        <div className="key" onClick={() => handleClick("q")}>
+          <div className="key-content">Q</div>
+        </div>
+        <div className="key" onClick={() => handleClick("w")}>
+          <div className="key-content">W</div>
+        </div>
+        <div className="key" onClick={() => handleClick("e")}>
+          <div className="key-content">E</div>
+        </div>
+        <div className="key" onClick={() => handleClick("r")}>
+          <div className="key-content">R</div>
+        </div>
+        <div className="key" onClick={() => handleClick("t")}>
+          <div className="key-content">T</div>
+        </div>
+        <div className="key" onClick={() => handleClick("y")}>
+          <div className="key-content">Y</div>
+        </div>
+        <div className="key" onClick={() => handleClick("u")}>
+          <div className="key-content">U</div>
+        </div>
+        <div className="key" onClick={() => handleClick("i")}>
+          <div className="key-content">I</div>
+        </div>
+        <div className="key" onClick={() => handleClick("o")}>
+          <div className="key-content">O</div>
+        </div>
+        <div className="key" onClick={() => handleClick("p")}>
+          <div className="key-content">P</div>
+        </div>
+      </div>
+      <div className="box-inner">
+        <div className="key" onClick={() => handleClick("a")}>
+          <div className="key-content">A</div>
+        </div>
+        <div className="key" onClick={() => handleClick("s")}>
+          <div className="key-content">S</div>
+        </div>
+        <div className="key" onClick={() => handleClick("d")}>
+          <div className="key-content">D</div>
+        </div>
+        <div className="key" onClick={() => handleClick("f")}>
+          <div className="key-content">F</div>
+        </div>
+        <div className="key" onClick={() => handleClick("g")}>
+          <div className="key-content">G</div>
+        </div>
+        <div className="key" onClick={() => handleClick("h")}>
+          <div className="key-content">H</div>
+        </div>
+        <div className="key" onClick={() => handleClick("j")}>
+          <div className="key-content">J</div>
+        </div>
+        <div className="key" onClick={() => handleClick("k")}>
+          <div className="key-content">K</div>
+        </div>
+        <div className="key" onClick={() => handleClick("l")}>
+          <div className="key-content">L</div>
+        </div>
+      </div>
+      <div className="box-inner">
+        <div className="key" onClick={() => handleDelete()}>
+          <div className="key-content">
+            <i class="fa-solid fa-delete-left"></i>
+          </div>
+        </div>
+        <div className="key" onClick={() => handleClick("z")}>
+          <div className="key-content">Z</div>
+        </div>
+        <div className="key" onClick={() => handleClick("x")}>
+          <div className="key-content">X</div>
+        </div>
+        <div className="key" onClick={() => handleClick("c")}>
+          <div className="key-content">C</div>
+        </div>
+        <div className="key" onClick={() => handleClick("v")}>
+          <div className="key-content">V</div>
+        </div>
+        <div className="key" onClick={() => handleClick("b")}>
+          <div className="key-content">B</div>
+        </div>
+        <div className="key" onClick={() => handleClick("n")}>
+          <div className="key-content">N</div>
+        </div>
+        <div className="key" onClick={() => handleClick("m")}>
+          <div className="key-content">M</div>
+        </div>
+        <div className="key" onClick={() => handleEnter()}>
+          <div className="key-content">
+            <i class="fa-regular fa-paper-plane"></i>
+          </div>
+        </div>
+      </div>
+      <a href="https://www.linkedin.com/in/sara-moreira-g">@saramordev</a>
+    </div>
+  );
+}
+
+export default Keyboard;
